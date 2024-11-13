57e9b177c257ea973547384eace90619581efa01
Hugo Ljungqvist <hulj07001@utb.vaxjo.se>
2024-11-13 12:11:53 +0100

    Add project files.

---
+++ inlämning3_2_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace inlämning3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Hej! Vi söker ny personal på vårat företag.");
+                Console.WriteLine("Har du gått ut gymnasiet? ( j / n )");
+                string gåttut = Console.ReadLine();
+                Console.WriteLine("Hur gammal är du?");
+                int ålder = int.Parse(Console.ReadLine());
+
+                if (gåttut == "j" && ålder < 22)
+                {
+                    Console.WriteLine("Vill vill gärna anställa dig!");
+                }
+                else
+                {
+                    Console.WriteLine("Vi söker tyvärr annan personal just nu.");
+                }
+            }
+        }
+    }
+}
